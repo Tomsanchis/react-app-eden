82a898f54eaf8e84988952f5810552b8249f9cde
Tom <tsanchis@edenschool.fr>
2022-06-14 10:22:48 +0200

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,16 @@ import React, { useState } from "react";
 const Home = () => {
   const [count, setcount] = useState(0);
   const [random, setrandom] = useState(0);
-  const [input, setinput] = useState("qsdqsdqsdqs")
-
+  const [input, setinput] = useState("");
+  const [text, settext] = useState("");
   const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+  let number = [2,1,0,5,7,9,52,12,45,31];
+
+  const listItems = number.map((number) =>
+  <li>{number}</li>
+);
 
   return (
     <main>
@@ -26,6 +31,19 @@ const Home = () => {
           setinput(e.target.value)
         }}/>
         {input.length < 12 ? <p>❌</p> : <p>✅</p>}
+      </div>
+      <div>
+        <p>Type Text</p>
+        <input type="text"  placeholder="type text" onChange={e => {
+          settext(e.target.value);
+        }}/>
+        <p>Render</p>
+        <p  style={{color: text.length < 8 ? "red" : "green"}}>{text}</p>
+      </div>
+      <div>
+        <div>
+          <p>{listItems}</p>
+        </div>
       </div>
     </main>
   );
